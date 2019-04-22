50ca515aa04bb68d51c28e83dddaf02743e70512
anajd <ana.jaime.diaz@gmail.com>
2019-04-22 12:16:19 +0200

    add image to show

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -3,51 +3,45 @@
 const searchInput = document.querySelector('.search__input');
 const searchButton = document.querySelector('.search__button');
 const resultList = document.querySelector('.result__list');
-// const resultContainer = document.querySelector('.result__container');
-const urlData = 'http://api.tvmaze.com/search/shows?q=nova';
+const imageSust = 'https://via.placeholder.com/210x295/ffffff/666666/?text=TV';
+const api = 'http://api.tvmaze.com/search/shows?q=';
 
-// Al hacer click
-// recogido en el input
-// muestra el titulo y cartel
-const fetchShows = url => {
-  fetch(url)
-    .then(response => response.json())
-    .then(data => {
-      for (let i = 0; i < data.length; i++) {
-        console.log(data[i].show.name);
-        const containerShow = document.createElement('li');
-        containerShow.classList.add('container__show');
-        const titleShow = document.createElement('h2');
-        titleShow.classList.add('title__show');
-        const imageShow = document.createElement('img');
-        imageShow.classList.add('image__show');
-        imageShow.src = data[i].show.image.medium;
-        imageShow.alt = data[i].show.name;
+const showClick = () => {
+  const term = searchInput.value;
+  const url = api + term;
 
-        const titleShowContent = document.createTextNode(data[i].show.name);
+  const fetchShows = url => {
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        for (let i = 0; i < data.length; i++) {
+          const containerShow = document.createElement('li');
+          containerShow.classList.add('container__show');
+          const titleShow = document.createElement('h2');
+          titleShow.classList.add('title__show');
+          const imageShow = document.createElement('img');
+          imageShow.classList.add('image__show');
+          const dataImg = data[i].show.image;
+          if (dataImg === null) {
+            imageShow.src = imageSust;
+          } else {
+            imageShow.src = dataImg.medium;
+          }
+          imageShow.alt = data[i].show.name;
 
-        titleShow.appendChild(titleShowContent);
+          const titleShowContent = document.createTextNode(data[i].show.name);
 
-        containerShow.appendChild(imageShow);
-        containerShow.appendChild(titleShow);
+          titleShow.appendChild(titleShowContent);
 
-        resultList.appendChild(containerShow);
-      }
-    });
+          containerShow.appendChild(imageShow);
+          containerShow.appendChild(titleShow);
+
+          resultList.appendChild(containerShow);
+        }
+      });
+  };
+
+  fetchShows(url);
 };
 
-fetchShows(urlData);
-
-const searchShow = () => {
-  const titleShow = document.querySelectorAll('.title__show');
-  for (const title of titleShow) {
-    const titleSearch = title.innerHTML;
-    if (titleSearch.includes(searchInput.value)) {
-      title.parentElement.classList.remove('hidden');
-    } else {
-      title.parentElement.classList.add('hidden');
-    }
-  }
-};
-
-searchButton.addEventListener('click', searchShow);
+searchButton.addEventListener('click', showClick);
